0a840d93687891a21bbd15210d3bde3bd39ebb28
Lucas <lapmrj@gmail.com>
2023-08-07 23:40:03 -0300

    add FriendshipModel and FriendRequestModel logic

--- server/src/models/FriendRequestModel.ts
+++ server/src/models/FriendRequestModel.ts
@@ -1,12 +1,19 @@
 import app from "../app"
 
+interface bodyInterface{
+  receiverId: string,
+  senderId: string,
+  requestResponse: boolean
+  friendRequestId: number
+}
+
 class FriendRequest{
-  public body
+  public body: bodyInterface
   public errors: String[]
   public response: any
   public prisma: typeof app.prisma
   
-  constructor(body: any){
+  constructor(body: bodyInterface){
     this.body = body
     this.errors = []
     this.response = null
@@ -14,14 +21,50 @@ class FriendRequest{
   }
 
   public async sendFriendRequest(){
+    if(!this.body.senderId || !this.body.receiverId) return this.errors.push("Informações não recebidas")
 
+    this.response = await this.prisma.friendRequest.create({
+      data:{
+        senderId: this.body.senderId,
+        receiverId: this.body.receiverId
+      }
+    }).catch(() => {
+      this.errors.push("solicitação de amizade já enviada")
+      return null
+    })
   }
 
   public async getFriendRequests(){
-
+    if(!this.body.receiverId) return this.errors.push("ID do usuario não recebida")
+    this.response = await this.prisma.friendRequest.findMany({where: {
+      receiverId: this.body.receiverId
+    }}).catch(() => this.errors.push("erro ao tentar encontrar solicitação de amizade"))
+    if(this.errors.length > 0) return
+    if(!this.response) return this.errors.push("Usuario não existe")
   }
-  public async handleRequestResponse(){
 
+  public async handleRequestResponse(){
+    if(!this.body.receiverId ||
+       !this.body.senderId ||
+       !this.body.requestResponse) return this.errors.push("Alguma informação está faltando") 
+    if(this.body.requestResponse) {
+      await this.prisma.friendship.create({
+        data:{
+          user_id: this.body.senderId,
+          friend_id: this.body.receiverId
+        }
+      }).catch(() => this.errors.push("solicitação de amizade não encontrada"))
+    }
+    if(this.errors.length > 0) return
+    return this.response = await this.removeFriendRequest()
+  }
+
+  private async removeFriendRequest(){
+    return this.prisma.friendRequest.delete({
+      where: {
+        id: this.body.friendRequestId 
+      }
+    }).catch(() => this.errors.push("Erro ao remover solicitação de amizade"))
   }
 }
 
---
+++ server/src/models/FriendshipModel.ts
@@ -0,0 +1,71 @@
+import app from "../app"
+
+
+interface bodyInterface{
+  userId: string,
+  friendId: string,
+  friendshipId: string
+}
+class Friendship{
+  public body: bodyInterface
+  public errors: String[]
+  public response: any
+  public prisma: typeof app.prisma
+  
+  constructor(body: bodyInterface){
+    this.body = body
+    this.errors = []
+    this.response = null
+    this.prisma = app.prisma
+  }
+
+  public async getFriendships(){
+    if(!this.body.userId) return this.errors.push("Id do usuário não recebido")
+
+    this.response = await this.prisma.friendship.findMany({where: {
+      friend_id: this.body.userId
+      }}).catch(() => this.errors.push("problema tentando achar amigos do usuario"))
+      if(this.errors.length > 0) return
+      this.response = [...this.response  ,...await this.prisma.friendship.findMany({where: {
+        user_id: this.body.userId
+      }}).catch(() => {
+        this.errors.push("problema tentando achar amigos do usuario")
+        return []
+      })]
+      if(this.errors.length > 0) return
+      if(this.response.length < 1) return this.errors.push("Você não tem amigos")
+  }
+
+  public async addFriendship(){
+    if(!this.body.friendId || !this.body.userId) this.errors.push("informações dos usuários não recebidas")
+    this.response = this.prisma.friendship.create({
+      data: {
+        friend_id: this.body.friendId,
+        user_id: this.body.userId
+      }
+    }).catch(() => {
+      this.errors.push("Erro ao gerar amizade")
+    })
+  }
+  public async getSingleFriendship(){
+    if(!this.body.friendshipId)
+    this.response = await this.prisma.friendship.findFirst({where: {
+      id: this.body.friendshipId
+    }}).catch(() => this.errors.push("Amizade não existe"))
+    if(this.errors.length > 0) return
+    if(!this.response) this.errors.push("amizade não encontrada")
+  }
+  public async removeFriendship(){
+    if(!this.body.friendshipId) return this.errors.push("id da amizade não recebido")
+
+    this.response = this.prisma.friendship.delete({where: {
+      id: this.body.friendshipId
+    }}).catch(() => {
+      this.errors.push("Amizade não encontrada")
+    })
+    if(this.errors.length > 0) return
+    if(!this.response) this.errors.push("Amizade não encontrada")
+  }
+}
+
+export default Friendship
